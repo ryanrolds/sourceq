655e6f2cfe7bbf7ee2f80af28f9ff2fb23dffe4d
Ryan Olds <ryanrolds@gmail.com>
2012-01-17 12:02:35 -0600

    Test(s)

---
+++ test/connection.js
@@ -0,0 +1,53 @@
+
+var should = require('should');
+
+var sq = require('../lib/client');
+
+//var host = 'twgamerzclassic.game.nfoservers.com';
+//var host = 'xeno2.game.nfoservers.com';
+var host = 'cDS.game.nfoservers.com';
+var port = 27015;
+
+describe('client', function() {
+  it('should take host port', function() {
+    var client = new sq(host, port);
+    client.host.should.equal(host);
+    client.port.should.equal(port);
+    client.close();
+  });
+
+  /*
+  it('should get info', function(done) {
+    this.timeout(10000);
+    var client = new sq(host, port);
+    client.getInfo(function(error, result) {
+      should.not.exist(error);
+      result.type.should.equal('I');
+      done();
+    });
+  });
+
+  it('should challenge', function(done) {
+    this.timeout(10000);
+    var client = new sq(host, port);
+    client.getChallenge(function(error, result) {
+      should.not.exist(error);
+      result.type.should.equal('A');
+      result.challenge.should.be.a('number');
+      done();
+    });
+  });
+  */
+
+  it('should get players', function(done) {
+    this.timeout(10000);
+    var client = new sq(host, port);
+    client.getPlayers(function(error, result) {
+      console.log(error, result);
+
+      should.not.exist(error);
+      result.type.should.equal('D');
+      done();
+    });
+  });
+});
