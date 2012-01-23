9cdfeca3afa6c665b336dd299a229d8fdd07dcb0
Ryan Olds <ryanrolds@gmail.com>
2012-01-23 04:13:25 -0600

    More changes. Player list works. Started on rules

--- test/client.test.js
+++ test/client.test.js
@@ -19,7 +19,6 @@ describe('client', function() {
     });
   });
 
-  /*
   it('should challenge', function(done) {
     this.timeout(10000);
     var client = new sq(host, port);
@@ -30,16 +29,25 @@ describe('client', function() {
       done();
     });
   });
-  */
 
   it('should get players', function(done) {
     this.timeout(10000);
     var client = new sq(host, port);
     client.getPlayers(function(error, result) {
-      console.log(error, result);
+      result.type.should.equal('D');
+      result.numPlayers.should.be.a('number');
+      result.players.should.be.an.instanceof(Array);
+      done();
+    });
+  });
 
-      should.not.exist(error);
-      result.type.should.equal('A');
+  it('should get rules', function(done) {
+    this.timeout(10000);
+    var client = new sq(host, port);
+    client.getRules(function(error, result) {
+      result.type.should.equal('E');
+      result.numRules.should.be.a('number');
+      result.rules.should.be.an.instanceof(Array);
       done();
     });
   });
